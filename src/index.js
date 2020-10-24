cde2ee5f9b09baec02fc80f968713d3f6e871246
Stasya Geleysha <s.geleisha@gmail.com>
2020-10-24 17:03:38 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = String(parseFloat(n));
+            
+    let result = str.split('').reverse().map((elem)=>(elem != "-")? elem= elem: elem ="").join("");  
+     
+    return result;
 }
